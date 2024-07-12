a42e326a5de8583d38f04712a8df6e42806c74d1
CihatAltiparmak <cihataltiparmak1@gmail.com>
2024-07-12 14:27:54 +0300

    Update data.js

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -32,6 +32,36 @@ window.BENCHMARK_DATA = {
             "extra": "iterations: 1\ncpu: 1373164583 ns\nthreads: 1"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cihataltiparmak1@gmail.com",
+            "name": "CihatAltiparmak",
+            "username": "CihatAltiparmak"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6ef257950f68ccc040d34d63664daa65ba155557",
+          "message": "Update run_benchmarks.yml",
+          "timestamp": "2024-07-11T01:07:20+03:00",
+          "tree_id": "a92c5d985f816894440671794babf0d11bbeee3f",
+          "url": "https://github.com/CihatAltiparmak/dummy_ci/commit/6ef257950f68ccc040d34d63664daa65ba155557"
+        },
+        "date": 1720649972281,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "ScenarioPerceptionPipelineFixture/test_scenario_perception_pipeline",
+            "value": 20681483859,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 1373164583 ns\nthreads: 1"
+          }
+        ]
       }
     ]
   }
