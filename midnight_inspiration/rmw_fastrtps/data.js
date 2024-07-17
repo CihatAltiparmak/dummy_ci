207df4bb0e587a9d01e5f39271d1c732828abb07
github-action-benchmark <github@users.noreply.github.com>
2024-07-17 21:29:12 +0000

    add Perception Pipeline Benchmark (googlecpp) benchmark result for 32cf67e19066a7c93c61d8da246e678791af45ac

---
+++ midnight_inspiration/rmw_fastrtps/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1721251752727,
+  "repoUrl": "https://github.com/CihatAltiparmak/dummy_ci",
+  "entries": {
+    "Perception Pipeline Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "cihataltiparmak1@gmail.com",
+            "name": "CihatAltiparmak",
+            "username": "CihatAltiparmak"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "32cf67e19066a7c93c61d8da246e678791af45ac",
+          "message": "Update run_benchmarks.yml",
+          "timestamp": "2024-07-18T00:24:24+03:00",
+          "tree_id": "52950d7b8748da55b27ee85fb2ac4b4c18b91254",
+          "url": "https://github.com/CihatAltiparmak/dummy_ci/commit/32cf67e19066a7c93c61d8da246e678791af45ac"
+        },
+        "date": 1721251752381,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "ScenarioPerceptionPipelineFixture/test_scenario_perception_pipeline",
+            "value": 195749074930,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 1291643024 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
